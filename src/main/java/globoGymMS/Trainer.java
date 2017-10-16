e641f263c6dfaa8aeca7bc7f53ef3e8604e3f346
Ashton Creel <ashton.creel@gmail.com>
2017-10-15 23:02:43 -0600

    Add username and password getters and setters

--- src/main/java/globoGymMS/Trainer.java
+++ src/main/java/globoGymMS/Trainer.java
@@ -32,6 +32,15 @@ public class Trainer {
 	}
 	
 	/* Start of getters & setters */
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	public String getName() {
 		return name;
 	}
