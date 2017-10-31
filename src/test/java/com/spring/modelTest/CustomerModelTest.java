25f594bc4832d781570c1e42a7e7e61e2f0ae730
Ashton Creel <ashton.creel@gmail.com>
2017-10-31 08:13:49 -0600

    Remove class copy in model test classes

--- src/test/java/com/spring/modelTest/CustomerModelTest.java
+++ src/test/java/com/spring/modelTest/CustomerModelTest.java
@@ -6,50 +6,6 @@ import org.junit.runner.RunWith;
 //import junit.framework.Assert;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
-
-//import com.spring.dao.CustomerDao;
-//import com.spring.model.Customer;
-//import com.spring.model.MembershipStatus;
-
-@ContextConfiguration(locations = "classpath:application-context-test.xml")
-@RunWith(SpringJUnit4ClassRunner.class)
-public class CustomerModelTest {
-     
-//    @Autowired
-//    private CustomerDao customerDao;
-     
-    @Test
-    @Transactional
-    @Rollback(true)
-    public void testAddCustomer() {
-//        Customer customer = new Customer();
-//        customer.setCustomerId(1);
-//        customer.setName("name");
-//        customer.setLastName("lastName");
-//        customer.setCustomerCustomerId(12);
-//        customer.setPhoneNumber(1234567890);
-//        customer.setEmail("email@email.com");
-//        customer.setInsurance("Aetna");
-//        customer.setMembership(MembershipStatus.ACTIVE);
-//        customerDao.addCustomer(customer);
-//         
-//        List<Customer> customers = customerDao.listCustomers();
-//         
-//        Assert.assertEquals(1, customers.size());         
-//        Assert.assertEquals(customer.getName(), customers.get(0).getName());
-    }
-}
-package com.spring.modelTest;
-
-//import java.util.List;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-//import junit.framework.Assert;
-//import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
--- src/test/java/com/spring/modelTest/WorkScheduleModelTest.java
+++ src/test/java/com/spring/modelTest/WorkScheduleModelTest.java
@@ -6,50 +6,6 @@ import org.junit.runner.RunWith;
 //import junit.framework.Assert;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
-
-//import com.spring.dao.WorkScheduleDao;
-//import com.spring.model.WorkSchedule;
-//import com.spring.model.MembershipStatus;
-
-@ContextConfiguration(locations = "classpath:application-context-test.xml")
-@RunWith(SpringJUnit4ClassRunner.class)
-public class WorkScheduleModelTest {
-     
-//    @Autowired
-//    private WorkScheduleDao workScheduleDao;
-     
-    @Test
-    @Transactional
-    @Rollback(true)
-    public void testAddWorkSchedule() {
-//        WorkSchedule workSchedule = new WorkSchedule();
-//        workSchedule.setWorkScheduleId(1);
-//        workSchedule.setName("name");
-//        workSchedule.setLastName("lastName");
-//        workSchedule.setWorkScheduleWorkScheduleId(12);
-//        workSchedule.setPhoneNumber(1234567890);
-//        workSchedule.setEmail("email@email.com");
-//        workSchedule.setInsurance("Aetna");
-//        workSchedule.setMembership(MembershipStatus.ACTIVE);
-//        workScheduleDao.addWorkSchedule(workSchedule);
-//         
-//        List<WorkSchedule> workSchedules = workScheduleDao.listWorkSchedules();
-//         
-//        Assert.assertEquals(1, workSchedules.size());         
-//        Assert.assertEquals(workSchedule.getName(), workSchedules.get(0).getName());
-    }
-}
-package com.spring.modelTest;
-
-//import java.util.List;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-//import junit.framework.Assert;
-//import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
