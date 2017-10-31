1b7b7484faecec06248001e7755bd16bf9cb7a20
Ashton Creel <ashton.creel@gmail.com>
2017-10-31 12:57:34 -0600

    WIP add external class to aid in model test

--- src/test/java/com/spring/modelTest/AddressTest.java
+++ src/test/java/com/spring/modelTest/AddressTest.java
@@ -3,11 +3,15 @@ package com.spring.modelTest;
 import org.junit.Test;
 import org.junit.Assert;
 import org.junit.runner.RunWith;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 //import junit.framework.Assert;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.spring.model.Address;
 
 //import com.spring.dao.AddressDao;
 //import com.spring.model.Address;
@@ -22,24 +26,4 @@ public class AddressTest {
 				Address.class, Entity.class, Table.class);
   }
      
-    @Test
-    @Transactional
-    @Rollback(true)
-    public void testAddAddress() {
-//        Address address = new Address();
-//        address.setAddressId(1);
-//        address.setName("name");
-//        address.setLastName("lastName");
-//        address.setAddressAddressId(12);
-//        address.setPhoneNumber(1234567890);
-//        address.setEmail("email@email.com");
-//        address.setInsurance("Aetna");
-//        address.setMembership(MembershipStatus.ACTIVE);
-//        addressDao.addAddress(address);
-//         
-//        List<Address> addresss = addressDao.listAddresss();
-//         
-//        Assert.assertEquals(1, addresss.size());         
-//        Assert.assertEquals(address.getName(), addresss.get(0).getName());
-    }
 }
