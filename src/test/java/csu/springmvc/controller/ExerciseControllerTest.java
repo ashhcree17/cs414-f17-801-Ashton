e7d44f1cc81bebfae168b5218d75d74dc082c66d
Ashton Creel <ashton.creel@gmail.com>
2017-11-23 11:39:59 -0700

    Create exercise controller tests for POST methods

--- src/test/java/csu/springmvc/controller/ExerciseControllerTest.java
+++ src/test/java/csu/springmvc/controller/ExerciseControllerTest.java
@@ -1,10 +1,38 @@
 package csu.springmvc.controller;
 
+import static org.hamcrest.CoreMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
+@ContextConfiguration(locations = { "classpath:csu/config/beans.xml" })
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
 public class ExerciseControllerTest {
+  
+  @Autowired
+  private WebApplicationContext wac;
+
+  private MockMvc mockMvc;
+  
+  @Before
+  public void setup() {
+      this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+  }
   
   @Test
   public void testCreateExerciseDuration() {
@@ -18,5 +46,30 @@ public class ExerciseControllerTest {
       ExerciseController exerciseController = new ExerciseController();
       ModelAndView mav = exerciseController.createExerciseSets(null, null);
       Assert.assertEquals("createExerciseSets", mav.getViewName());
+  }
+
+  @Test
+  public void testCreateExerciseDurationProcess() throws Exception {
+    this.mockMvc.perform(post("/createExerciseDurationProcess")
+        .param("exerciseid", "78901")
+        .param("name", "Dumbbell")
+        .param("duration", "120")
+          )
+        .andExpect(status().isOk())
+        .andExpect(forwardedUrl("success"))
+        .andExpect(model().attribute("name", any(String.class)));
+  }
+
+  @Test
+  public void testCreateExerciseSetsProcess() throws Exception {
+    this.mockMvc.perform(post("/createExerciseSetsProcess")
+        .param("exerciseid", "78902")
+        .param("name", "Dumbbell")
+        .param("numberOfSets", "3")
+        .param("repsPerSet", "12")
+          )
+        .andExpect(status().isOk())
+        .andExpect(forwardedUrl("success"))
+        .andExpect(model().attribute("name", any(String.class)));
   }
 }
