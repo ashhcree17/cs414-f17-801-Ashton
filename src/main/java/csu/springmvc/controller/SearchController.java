601b33d10cf503124184dde75efef519ea92fe98
Ashton Creel <ashton.creel@gmail.com>
2017-11-28 13:24:45 -0700

    WIP update search controller and view

--- src/main/java/csu/springmvc/controller/SearchController.java
+++ src/main/java/csu/springmvc/controller/SearchController.java
@@ -2,8 +2,12 @@ package csu.springmvc.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,11 +22,10 @@ public class SearchController {
     return mav;
   }
 
-  @RequestMapping(value = "/searchProcess", method = RequestMethod.POST)
-  public ModelAndView searchProcess(HttpServletRequest request, HttpServletResponse response,
-      @RequestParam("searchTerm") String searchTerm) {
-    ModelAndView mav = null;
-
-    return mav;
-  }
+//  @RequestMapping(value = "/searchProcess", method = RequestMethod.GET)
+//  public String searchProcessByAddress(@PathVariable int addressid,
+//      Model model) {
+//    model.addAttribute("search", addressService.getAddress(addressid));
+//    return "addressList";
+//  }
 }
--- src/main/webapp/jsp/searchView.jsp
+++ src/main/webapp/jsp/searchView.jsp
@@ -1,20 +1,12 @@
-<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<title>Search</title>
+		<title>Insert title here</title>
 	</head>
 	<body>
-		<form:form method="post" action="searchProcess">
-			<table align="center">
-				<tr>
-					<td><form:input path="command" /></td>
-					<td><input type="submit" /></td>
-				</tr>
-			</table>
-		</form:form>
+	
 	</body>
 </html>
